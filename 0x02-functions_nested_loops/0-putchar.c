c4e54f1f86928a0a1b212e3ea3babf816100333b
super-3ero <57857348+super-3ero@users.noreply.github.com>
2022-03-15 22:11:20 +0300

    fixed code error

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,15 @@
 
 int main(void)
 {
-char str[] ="putchar"
+char str[] ="putchar";
 int i=0;
-while (i<7)
+while (i < 7)
 	{
 
 	_putchar(str[i]);
 	i++;
 	}
-	_putchar('\n')
+	_putchar('\n');
 return (0);
 }
 
