f770b1c2dccddb8512c00d0999c09a82c7b7dda9
super-3ero <57857348+super-3ero@users.noreply.github.com>
2022-03-15 22:23:55 +0300

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,18 @@
  * in lowecase followed by a new line
  * Return: void
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-	char letter = 'a';
+	int i;
 
-	while (letter <= 'z')
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(letter);
-		letter++;
+		char ch;
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
