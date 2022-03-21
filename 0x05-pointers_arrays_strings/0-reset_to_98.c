6371a00eea874a35981d812b2ec5693240f67f93
super-3ero <57857348+super-3ero@users.noreply.github.com>
2022-03-21 13:39:15 +0300

    fixed codding style error

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,12 @@
 #include "main.h"
 
 /**
- * Resets value to 98
+ * reset_to_98 - updates the value of n to 98
+ * @n: integer pointer to a variable to be updated
+ *
+ * Return: nothing
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
